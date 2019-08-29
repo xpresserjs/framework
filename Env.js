5d52f76db9334e27867947168de427a1d588f0d4
Evil <lordcashtugbaski@gmail.com>
2019-08-29 04:18:55 +0100

    Added: Env.js .env file loader.

---
+++ Env.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const dotenv = require("dotenv");
+
+/**
+ * @param {string} path
+ * @returns {*}
+ */
+module.exports = (path) => {
+
+    if (!fs.existsSync(path)) {
+        return throw new Error(`Env file: {${path}} does not exists!`);
+    }
+
+    if (fs.lstatSync(path).isDirectory()) {
+        path = path + "/.env";
+    }
+
+    return dotenv.config({path}).parsed;
+};
