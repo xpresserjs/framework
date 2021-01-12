26372b20ba8d08df6a8169f47de1da7349f0aaf7
trapcodeio <trapcode.io@gmail.com>
2021-01-12 05:28:09 +0100

    Added: Conditional Helpers

--- src/Helpers/Conditions.ts
+++ src/Helpers/Conditions.ts
@@ -1,3 +1,46 @@
-export function ifNotDefined(item: any): any {
-    return true;
+/**
+ * Check if a variable is null or undefined.
+ * @param item
+ */
+export function isNullOrUndefined(item: any) {
+    return item === null || item === undefined;
+}
+
+/**
+ * Checks if variable is null or undefined.
+ *
+ * Returns variable value if defined else returns value of alt variable or function.
+ * @param variable
+ * @param alt
+ */
+export function ifNotDefined<T>(variable: T | (() => T), alt: T | (() => T)): T {
+    if (typeof variable === "function") {
+        variable = (variable as Function)();
+    }
+
+    if (isNullOrUndefined(variable) && typeof alt === "function") {
+        alt = (alt as Function)();
+    }
+
+    return (isNullOrUndefined(variable) ? alt : variable) as T;
+}
+
+
+/**
+ * Checks if variable is defined.
+ *
+ * Returns variable value if defined else returns value of alt variable or function.
+ * @param variable
+ * @param alt
+ */
+export function ifOrElse<T>(variable: T | (() => T), alt: T | (() => T)): T {
+    if (typeof variable === "function") {
+        variable = (variable as Function)();
+    }
+
+    if (!variable && typeof alt === "function") {
+        alt = (alt as Function)();
+    }
+
+    return (variable ? variable : alt) as T;
 }
