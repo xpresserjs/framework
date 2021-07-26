0c890d8a4996d0c114094b847a2f703b8396c511
trapcodeio <trapcode.io@gmail.com>
2021-07-26 13:38:21 +0100

    Refactored Events Loader

--- src/Events/Loader.ts
+++ src/Events/Loader.ts
@@ -7,64 +7,62 @@ import {getInstance} from "../../index";
 const $ = getInstance();
 const DefinedEvents: Record<string, any> = {};
 const EventsPath = $.path.events();
+// Load all event files
+const $useDotJson: ObjectCollection = $.engineData.get("UseDotJson");
+const excludedFiles = $useDotJson.get("exclude.events.files", []);
+const excludedFolders = $useDotJson.get("exclude.events.folders", []);
+Path.addProjectFileExtension(excludedFiles);
 
-if (fs.existsSync(EventsPath)) {
+function readEventsDirectory (parent: string) {
+    const Events = fs.readdirSync(parent);
 
-    // Load all event files
-    const $useDotJson: ObjectCollection = $.engineData.get("UseDotJson");
-    const excludedFiles = $useDotJson.get("exclude.events.files", []);
-    const excludedFolders = $useDotJson.get("exclude.events.folders", []);
-    Path.addProjectFileExtension(excludedFiles);
+    for (let i = 0; i < Events.length; i++) {
+        const event: string[] | string = Events[i];
+        const fullPath = parent + "/" + event;
+        const shortPath = fullPath.replace(EventsPath + "/", "");
 
-    const readEventsDirectory = (parent: string) => {
-        const Events = fs.readdirSync(parent);
+        if (fs.lstatSync(fullPath).isDirectory()) {
+            // Get events not excluded in use.json
+            if (!excludedFolders.includes(event)) {
+                readEventsDirectory(fullPath);
+            }
+        } else {
+            // Get events not excluded in use.json
+            if (!excludedFiles.includes(event)) {
+                let event: any;
 
-        for (let i = 0; i < Events.length; i++) {
-            const event: string[] | string = Events[i];
-            const fullPath = parent + "/" + event;
-            const shortPath = fullPath.replace(EventsPath + "/", "");
-
-            if (fs.lstatSync(fullPath).isDirectory()) {
-                // Get events not excluded in use.json
-                if (!excludedFolders.includes(event)) {
-                    readEventsDirectory(fullPath);
+                try {
+                    event = require(fullPath);
+                } catch (e) {
+                    $.logPerLine([
+                        {error: `Error in ${shortPath}`},
+                        {error: e},
+                    ]);
                 }
-            } else {
-                // Get events not excluded in use.json
-                if (!excludedFiles.includes(event)) {
-                    let event: any;
 
-                    try {
-                        event = require(fullPath);
-                    } catch (e) {
-                        $.logPerLine([
-                            {error: `Error in ${shortPath}`},
-                            {error: e},
-                        ]);
-                    }
+                if (typeof event === "object") {
+                    const eventKeys = Object.keys(event);
+                    const namespace = event.namespace || Path.removeProjectFileExtension(shortPath);
 
-                    if (typeof event === "object") {
-                        const eventKeys = Object.keys(event);
-                        const namespace = event.namespace || Path.removeProjectFileExtension(shortPath);
+                    for (let j = 0; j < eventKeys.length; j++) {
+                        const eventKey = eventKeys[j];
+                        if (typeof event[eventKey] === "function") {
+                            let name = namespace;
 
-                        for (let j = 0; j < eventKeys.length; j++) {
-                            const eventKey = eventKeys[j];
-                            if (typeof event[eventKey] === "function") {
-                                let name = namespace;
-
-                                if (eventKey !== "index") {
-                                    name = `${namespace}.${eventKey}`;
-                                }
-
-                                DefinedEvents[name] = event[eventKey];
+                            if (eventKey !== "index") {
+                                name = `${namespace}.${eventKey}`;
                             }
+
+                            DefinedEvents[name] = event[eventKey];
                         }
                     }
                 }
             }
         }
-    };
+    }
+}
 
+if (fs.existsSync(EventsPath)) {
     $.ifConsole(() => {
         if ($.config.get('artisan.loadEvents')) {
             readEventsDirectory(EventsPath);
